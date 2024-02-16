cbf83e1e3cb3c724667443c6303b3073412304f1
mrsahadatarefin <sahadatarifin806@gmail.com>
2024-02-16 20:16:49 +0600

    cors path added

--- src/app.ts
+++ src/app.ts
@@ -6,8 +6,8 @@ import notFound from './app/middlewares/notFound';
 
 const app: Application = express();
 
-app.use(cors());
 app.use(express.json());
+app.use(cors({ origin: 'http://localhost:5173/' }));
 
 //routes
 
